627de4d10dbffaf09e21b3bd4f1b420dcd2fb9bf
Esteban Lagos <elagosquintanilla@gmail.com>
2022-08-10 21:44:57 -0400

    Navigate componente en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom';
 import Layout from "./layout/Layout";
 import Inicio from "./paginas/Inicio";
 import NuevoCliente from "./paginas/NuevoCliente";
